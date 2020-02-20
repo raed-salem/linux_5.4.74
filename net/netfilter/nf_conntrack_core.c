f039ca07c33bd7421a5e2e7981bfad8f51b3808c
Li RongQing <lirongqing@baidu.com>
2020-02-20 15:20:18 +0800

    netfilter: cleanup unused macro
    TEMPLATE_NULLS_VAL is not used after commit 0838aa7fcfcd
    ("netfilter: fix netns dependencies with conntrack templates")
    
    PFX is not used after commit 8bee4bad03c5b ("netfilter: xt
    extensions: use pr_<level>")
    
    Issue: 2346931
    Signed-off-by: Li RongQing <lirongqing@baidu.com>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- net/netfilter/nf_conntrack_core.c
+++ net/netfilter/nf_conntrack_core.c
@@ -2703,7 +2703,6 @@ void nf_conntrack_init_end(void)
  */
 #define UNCONFIRMED_NULLS_VAL	((1<<30)+0)
 #define DYING_NULLS_VAL		((1<<30)+1)
-#define TEMPLATE_NULLS_VAL	((1<<30)+2)
 
 int nf_conntrack_init_net(struct net *net)
 {
